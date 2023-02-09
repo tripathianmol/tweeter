508276537017e0dfc055ec7ef3dfacbe550cf573
Anmol Tripathi <tripathianmol@protonmail.com>
2023-02-09 16:22:06 -0600

    media inquiries added

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,68 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(() => {
+  $("#error").hide();
+
+  const escape = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  const createTweetElement = function(obj) {
+    const $tweet = $(`
+  <article class="tweet">
+  <header>
+    <div>
+      <i class="fa-solid fa-user-ninja"></i>
+      <span>${obj["user"]["name"]}</span>
+    </div>
+    <div>
+      <span>${obj["user"]["handle"]}</span>
+    </div>
+  </header>
+  <p>
+    ${escape(obj["content"]["text"])}
+  </p>
+  <footer>
+    <div>
+    ${timeago.format(obj["created_at"])}
+    </div>
+    <div>
+      <i class="fa-solid fa-flag icon"></i>
+      <i class="fa-solid fa-retweet icon"></i>
+      <i class="fa-solid fa-heart icon"></i>
+    </div>
+  </footer>
+</article>
+  `);
+
+    return $tweet;
+  };
+
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      let tweetHTML = createTweetElement(tweet);
+      $('#tweets').append(tweetHTML);
+    }
+  };
+
+  const loadTweets = function() {
+    $.get("/tweets", renderTweets);
+  };
+
+  loadTweets();
+
+  $('#form').submit(function(event) {
+    let tweet = $('#tweet-text').val();
+    
+    event.preventDefault();
+    if (tweet === "" || tweet.length > 140) {
+      alert("Tweet too long or empty.");
+      $("#error").show();
+    } else {
+      $("#error").hide();
+      $.post('/tweets', $('#form').serialize()).done(() => loadTweets());
+    }
+  });
+});
+
 
